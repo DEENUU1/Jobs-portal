d4fed733e06a868c8a66d785ca67038059d09c2f
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-16 18:43:22 +0200

    Feature | Contract model and updates for other models | Contract model allows to choose a type of contract for example b2b but company is able to choose more than 1 type of contract I also changes the Level and Position models by adding a default values to choose because they are const and there is no needed to change them

--- base/offers/models.py
+++ base/offers/models.py
@@ -2,11 +2,35 @@ from django.db import models
 
 
 class Position(models.Model):
-    name = models.CharField(max_length=50)
+    POSITION = (
+        ("python", "Python"),
+        ("java", "Java"),
+        ("c", "C"),
+        (".net", ".NET"),
+        ("javascript", "JavaScript"),
+        ("php", "PHP"),
+        ("scala", "Scala"),
+        ("mobile", "Mobile"),
+        ("testing", "Testing"),
+        ("devops", "DevOps"),
+        ("ux/ui", "UX/UI"),
+        ("game", "Game"),
+        ("data", "Data"),
+        ("golang", "Golang"),
+        ("security", "Security"),
+    )
+    position = models.ManyToManyField(choices=POSITION, max_length=50)
 
 
 class Level(models.Model):
-    name = models.CharField(max_length=50)
+    LEVEL = (
+        ("intern", "Intern"),
+        ("junior", "Junior"),
+        ("middle", "Middle"),
+        ("senior", "Senior"),
+        ("expert", "Expert")
+    )
+    name = models.CharField(choices=LEVEL, max_length=50)
 
 
 class Country(models.Model):
@@ -18,3 +42,11 @@ class Localization(models.Model):
     city = models.CharField(max_length=50)
 
 
+class Contract(models.Model):
+    CONTRACT = (
+        ("uop", "Umowa o pracę"),
+        ("uz", "Umowa zlecenie"),
+        ("b2b", "Kontrakt b2b"),
+    )
+
+    contract = models.ManyToManyField(choices=CONTRACT, max_length=50)
