b1e58dfea0d8e460e75f2feb36887f3abc95ab3a
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-16 15:09:30 +0200

    Feature | Basic users | ROLE -> company, user I added a few custom fields like: phone number with validation, image, description, first and last name While registration user can choose the type of an account

---
+++ base/accounts/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+import PIL
+
+
+class CustomUser(AbstractUser):
+
+    ROLE = (
+        ('company', 'company'),
+        ('user', 'user')
+    )
+
+    role = models.CharField(max_length=10, choices=ROLE)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+    description = models.CharField(max_length=500, null=True, blank=True)
+    image = models.ImageField(upload_to='images', null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.role == "company":
+            self.first_name = None
+            self.last_name = None
+
+        super().save(*args, **kwargs)
