3168ef22735f0b6c7842e4340398be0be35c7c1e
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-16 23:19:50 +0200

    Test | Level  model | Simple Test Case for Level  model that test creating a new object

--- base/offers/tests/test_models.py
+++ base/offers/tests/test_models.py
@@ -22,3 +22,15 @@ class PositionModelTestCase(TestCase):
         self.assertEqual(
             self.position.position_name, "Python"
         )
+
+
+class LevelModelTestCase(TestCase):
+    def setUp(self) -> None:
+        self.level = Level.objects.create(
+            level_name = "Junior"
+        )
+    
+    def test_level_model_creation(self):
+        self.assertEqual(
+            self.level.level_name, "Junior"
+        )
