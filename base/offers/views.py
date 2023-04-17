0ea7f51428f2e985d4d4b13f2182e71d71830869
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-17 12:14:44 +0200

    Feature | Position filter form ChoosePositionForm is a class which allows to display a form which multiply choice field It is used for filtering offers by positions

--- base/offers/views.py
+++ base/offers/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Offer
 from typing import Any , Dict 
 from django.db.models import QuerySet
+from .forms import ChoosePositionsForm
 
 
 class HomePageView(ListView):
@@ -13,9 +13,15 @@ class HomePageView(ListView):
     def get_queryset(self) -> QuerySet[Any]:
         queryset = super().get_queryset()
 
+        positions = self.request.GET.getlist('choose_positions')
+        if positions:
+            queryset = queryset.filter(position__in=positions)
+            
         return queryset
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context =super().get_context_data(**kwargs)            
+
+        context['positions_form'] = ChoosePositionsForm(self.request.GET)
 
         return context
