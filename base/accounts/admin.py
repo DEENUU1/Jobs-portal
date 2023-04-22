78ae278ff13b595fa39a7b17ef8c20388960158f
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-22 15:22:10 +0200

    Feature | Custom admin register | I used  admin.register decorator to register all available models and add list_filter for better transparency Also Offer model I used TabularInline class from django to dispay a list of applications in specified job offer

--- base/accounts/admin.py
+++ base/accounts/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'role',
+        'is_active',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'username'
+    ]
+
+    list_filter = [
+        'role',
+        'is_active'
+    ]
