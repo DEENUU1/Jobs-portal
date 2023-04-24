cec13bbca805222553f2bb2e21a16b77557cf00f
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-24 18:25:44 +0200

    Feature | study app models | Category model which anly has name field and represents a specified category of study field Resources is a model which store a information about some interestring thinks and has foreignkey to Category model

--- base/study/admin.py
+++ base/study/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Category, Resources
 
-# Register your models here.
+
+@admin.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name'
+    ]
+
+    list_filter = [
+        'name'
+    ]
+
+
+@admin.register(Resources)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name', 'category', 'url', 'date_created'
+    ]
+
+    list_filter = [
+        'name', 'category', 'date_created'
+    ]
