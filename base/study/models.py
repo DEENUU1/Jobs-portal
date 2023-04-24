177cc1f5f0e2ada9bd17268bdebc31c43e8cb236
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-24 18:51:27 +0200

    Feature | StudyHomePageListVIew | Main view to display a list of all objects from Resources model IT has a pagination set by default on 10 items View inherits from django generic ListView Later there will be a few filters added to get_queryset methods

--- base/study/models.py
+++ base/study/models.py
@@ -5,6 +5,14 @@ class Category(models.Model):
     """  """
     name = models.CharField(max_length=50)
 
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
 
 class Resources(models.Model):
     """ """
@@ -14,3 +22,10 @@ class Resources(models.Model):
     date_created = models.DateTimeField(auto_created=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Resource'
+        verbose_name_plural = 'Resources'
+
+    def __str__(self):
+        return self.name
--- base/study/views.py
+++ base/study/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from typing import Any, Dict
+from django.db.models import QuerySet
+from django.views.generic import ListView
+from .models import Category, Resources
 
-# Create your views here.
+
+class StudyHomePageListView(ListView):
+    """
+
+    """
+    model = Resources
+    template_name = 'study_home_page_list.html'
+    paginate_by = 10
+
+    def get_queryset(self) -> QuerySet[Any]:
+        """
+
+        """
+        queryset = super().get_queryset()
+        return queryset
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        """
+
+        """
+        context =super().get_context_data(**kwargs)
+
+        return context
