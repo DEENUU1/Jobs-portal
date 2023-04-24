f90d8da0aab5a624d357e8d125f6232a85c206a1
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-24 22:25:52 +0200

    Test | study view | Test for StudyHomePageListView to check the status code (should be 200) and template used to display that view

--- base/study/tests/test_views.py
+++ base/study/tests/test_views.py
@@ -1,5 +1,5 @@
-from django.test import TestCase, CLient
-from django.urls import resolve
+from django.test import TestCase, Client
+from django.urls import reverse
 
 
 class StudyViewsTestCase(TestCase):
@@ -7,7 +7,7 @@ class StudyViewsTestCase(TestCase):
         self.client = Client()
 
     def test_study_list_url_resolves(self):
-        response = self.client.get('study:study_list')
+        response = self.client.get(reverse('study:study_list'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'study_home_page_list.html')
 
